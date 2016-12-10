56257f29ddd214e60f1cc78bacbe32286faf3fee
soorism <omranpour@ce.sharif.ir>
2016-12-10 17:40:21 +0330

    main.c added

--- main.c
+++ main.c
@@ -1,21 +1,8 @@
 #include<stdio.h>
 
-int isPrime(int num) {
-    if (num == 1)
-        return 0;
-    else
-        for (i=2;i*i<=num;i++)
-            if (num % i == 0)
-                return 0;
-    return 1;
-}
-
-int absVal(int num) {
-
-}
-
 int main() {
-	printf("Hello world!");
+    char str;
+    scanf("%s",str);
 	printf("\nHello Worms!")
 	return 0;
 }
